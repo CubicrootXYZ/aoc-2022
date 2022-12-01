d4897ffffe2758cec7dbf54c1ea8b56eedbc9cd9
cubicroot <github@cubicroot.xyz>
2022-12-01 18:45:52 +0100

    day1: finish part 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -6,20 +6,36 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut calories = vec![vec![0; 0]; 0];
+    let mut cals = vec![0; 0];
     for line in reader.lines() {
         let cal = line?;
-        let mut cals = vec![0; 0];
 
-        if cal == "" {
+        if cal == "" && cals.len() > 0 {
             calories.push(cals);
 
             cals = vec![0; 0];
         } else {
             cals.push(cal.parse::<i32>().unwrap());
         }
-
-        println!("{}", calories.len());
     }
+    if cals.len() > 0 {
+        calories.push(cals)
+    }
+
+    let mut max_calories = 0;
+    for cals in calories.iter() {
+        let mut summed_calories = 0;
+
+        for cal in cals.iter() {
+            summed_calories += cal;
+        }
+
+        if summed_calories > max_calories {
+            max_calories = summed_calories;
+        }
+    }
+
+    println!("Max calories per raindeer: {}", max_calories);
 
     Ok(())
 }
